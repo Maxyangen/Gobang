88ad22cdc80c2e1ea43ca2c64d6f4dcfdaa6ec50
Maxgenyan <tunhaungaa@yahoo.com.tw>
2019-09-01 05:13:58 +0800

    center canvas and using new get mouse position method

--- gobangcontr.js
+++ gobangcontr.js
@@ -10,8 +10,8 @@ var over = false;
 
 
 function startGame() {
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    document.addEventListener("click", playerPutChess);
+    //document.addEventListener('contextmenu', event => event.preventDefault());
+    //document.addEventListener("click", playerPutChess);
     dialog = document.getElementById("dialog");
     initData();
     drawChessBoard();
@@ -33,6 +33,10 @@ function initData() {
 function drawChessBoard() {
     canvas = document.getElementById('canvas_id');
     canvasContext = canvas.getContext('2d');
+    //report the mouse position on click
+    canvas.addEventListener("click", playerPutChess);
+
+
 
 
     for (var i = 0; i <= 640; i += 40) {
@@ -52,29 +56,44 @@ function drawChessBoard() {
 
 }
 
+//Get Mouse Position
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 function playerPutChess(e) {
-    console.log("mouse click x coor" + e.clientX);
-    console.log("mouse click y coor" + e.clientY);
+    var mousePos = getMousePos(canvas, e);
+    //alert(mousePos.x + ',' + mousePos.y);
+    var newX = parseInt(mousePos.x / 37);
+    var newY = parseInt(mousePos.y / 37);
+    /*console.log("mouse click x coor" + e.clientX);
+    console.log("mouse click y coor" + e.clientY);*/
+
+    console.log("new mouse X " + newX);
+    console.log("new mouse Y " + newY);
 
 
     var x = parseInt(e.clientX / 40);
     var y = parseInt(e.clientY / 40);
 
     console.log(x + " " + y);
-    if (x <= 0 || x > 15 || y <= 0 || y > 15 || chessBoard[y][x] !== 0) {
+    if (newX <= 0 || newX > 15 || newY <= 0 || newY > 15 || chessBoard[newY][newX] !== 0) {
         alert("replay");
         return;
     }
 
-    drawChess(currentTeam.color, x * 40, y * 40);
-    chessBoard[y][x] = currentTeam;
-    checkWinner(currentTeam, y, x);
+    drawChess(currentTeam.color, newX* 40, newY* 40);
+    chessBoard[newY][newX] = currentTeam;
+    checkWinner(currentTeam, newY, newX);
 
     
     //currentTeam = currentTeam === BLACKCHESS ? WHITECHESS : BLACKCHESS;
     console.log(currentTeam);
     for (var k = 0; k < count; k++) {
-        if (wins[y][x][k]) {
+        if (wins[newY][newX][k]) {
             playerWin[k]++;
             computerWin[k] = 6;
             if (playerWin[k] == 5) {
@@ -172,6 +191,7 @@ function checkWinner(chessTeam, x, y) {
 
 
 }
+
 
 
 
